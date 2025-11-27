546c8a7c090a7125d1e034c1b080f0ab485c2e62
shanks <shanks.d039@gmail.com>
2025-11-27 16:49:13 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: './', // 这一行很重要，确保变成白屏
-  server: {
-    host: true // 允许局域网访问
-  }
+  base: '/money-app/', // <--- 这里一定要改成 '/您的仓库名/'，前后都要有斜杠
 })
